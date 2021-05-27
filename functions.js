0278a400c91c4e05fca25e5cd6c54f96693e5958
Nathan Presley <npresley13@gmail.com>
2021-05-27 00:43:29 -0400

    add javascript file

---
+++ functions.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+// let x = document.getElementById('hamburgerBtn');
+// x.onclick = toggleMenu;
+
+document.getElementById('hamburgerBtn').addEventListener(onclick, toggleMenu);
