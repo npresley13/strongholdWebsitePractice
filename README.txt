54198227cdcbfe81088ea87fece9370b21a96ba6
Nathan Presley <npresley@strong-hold.com>
2021-07-27 07:47:28 -0400

    fixed email validation issue

---
+++ README.txt
@@ -0,0 +1,24 @@
+My website is a simplified version of my companies website strong-hold.com.
+
+CSS Requirements
+
+Throughout the website I use several instances of flexbox and CSS grid. 
+If you navigate to the products page, you will see an example of CSS grid 
+used to organize my products. 
+You will also notice that I use an expandable menu depending on the screen
+size that you are viewing the website. 
+
+JavaScript Requirements
+
+If you navigate to "Quote" page, you will notice a form with form validation.
+See forms.js to view the code.
+The products on the products page is being populate by a JavaScript array.
+The menu used is being pulled into the menu by looping through the items 
+and displaying them as nav items.
+I also have set several variables using const.
+Them mobile menu switches between hamburger and times and shows the mobile
+menu.
+
+FYI 
+
+- The search bar is for styling and is not functioning
--- forms.js
+++ forms.js
@@ -1,5 +1,8 @@
 
 //First Name Validation -- Cannot be empty
+//Last Name Validation -- cannot be empty
+//Email Validation -- must follow the format of an email
+
 function formValidation() {
     let formInputFirstName = document.forms["quote-form"]["first-name"].value;
     let formInputLastName = document.forms["quote-form"]["last-name"].value;
@@ -10,26 +13,11 @@ function formValidation() {
     } else if (formInputLastName == "") {
         alert("Please provide last name.");
         return false;
-    } else if (formInputEmail !== "") {
+    } else if (/(.+)@(.+){2,}\.(.+){2,}/.test(formInputEmail)) {
+        return true;
+    } else {
         alert("Please provide valid email.");
         return false;
     }
 }
 
-// //Last Name Validation -- Cannot be empty
-// function formValidation() {
-//     let formInput = document.forms["quote-form"]["last-name"].value;
-//     if (formInput == "") {
-//         alert("Please provide last name.");
-//         return false;
-//     } 
-// }
-
-// //Email Validation
-// function formValidation() {
-//     let formInput = document.forms["quote-form"]["email"].value;
-//     if (formInput !== "/\S+@\S+\.\S+/") {
-//         alert("Please provide valid email.");
-//         return false;
-//     } 
-// }
